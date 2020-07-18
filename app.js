28143bed430e36605ef018abe9c8245271bafe47
jaisondza <jaisonj1010@gmail.com>
2020-07-18 14:56:54 +0530

    getting products

---
+++ app.js
@@ -0,0 +1,51 @@
+// variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+//cart
+let cart = [];
+
+//getting the products
+
+class Products {
+  async getProducts() {
+    try {
+      let results = await fetch("products.json");
+      let data = await results.json();
+      let products = data.items; // items is an array.
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { id, title, price, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+// display products
+class UI {}
+
+//local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+  //get all the products
+  products
+    .getProducts()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+});
