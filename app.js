3d8185a26165e8e6c995aa3bb4246f46081c3cdb
jaisondza <jaisonj1010@gmail.com>
2020-07-18 18:06:40 +0530

    displaying products

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
+
 //cart
 let cart = [];
 
@@ -31,10 +32,41 @@ class Products {
   }
 }
 // display products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach(item => {
+      result += `<article class="product">
+          <div class="img-container">
+            <img
+              src="${item.image}"
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id="${item.id}">
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${item.title}</h3>
+          <h4>$${item.price}</h4>
+        </article>`;
+    });
+    productsDOM.innerHTML = result;
+  }
+
+  // getting bag buttons
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll("bag-btn")];
+  }
+}
 
 //local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
@@ -42,8 +74,13 @@ document.addEventListener("DOMContentLoaded", () => {
   //get all the products
   products
     .getProducts()
-    .then(data => {
-      console.log(data);
+    .then(products => {
+      console.log(products);
+      ui.displayProducts(products);
+      Storage.saveProducts(products); // since the method is static, no need of an instance.
+    })
+    .then(() => {
+      ui.getBagButtons(); // calling here because bag buttons can be accessed only after loading the products.
     })
     .catch(e => {
       console.log(e);
