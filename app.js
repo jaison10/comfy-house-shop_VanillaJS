6fb8d3073eb9ce555998a43ba8acd97616192d0f
Jaison DSouza B <jaisonj1010@gmail.com>
2021-01-12 11:42:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const productsDOM = document.querySelector(".products-center");
 //scrolling on clicking shopnow button
 document.getElementById("shopnow").addEventListener("click", ()=>{
   window.scroll({
-  top: 450,
+  top: 480,
   behavior: 'smooth'
   });
 //   console.log("Has to scroll");
